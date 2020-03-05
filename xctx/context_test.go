a42ea099017e31bac6046fe33b092ff58daa2d65
lixiaohui <lixiaohui0812@gmail.com>
2020-03-05 23:00:38 +0800

    xctx: use require instead assert

--- xctx/context_test.go
+++ xctx/context_test.go
@@ -5,15 +5,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReadInt(t *testing.T) {
 	ctx := context.Background()
 	newCtx := context.WithValue(ctx, "int", 1)
 	i, ok := ReadInt(newCtx, "int")
-	assert.Equal(t, true, ok)
-	assert.Equal(t, 1, i, "store value is 1")
+	require.Equal(t, true, ok)
+	require.Equal(t, 1, i, "store value is 1")
 }
 
 func TestReadTime(t *testing.T) {
@@ -21,6 +21,6 @@ func TestReadTime(t *testing.T) {
 	n := time.Now()
 	newCtx := context.WithValue(ctx, "now", n)
 	now, ok := ReadTime(newCtx, "now")
-	assert.Equal(t, true, ok)
-	assert.Equal(t, n.UnixNano(), now.UnixNano())
+	require.Equal(t, true, ok)
+	require.Equal(t, n.UnixNano(), now.UnixNano())
 }
