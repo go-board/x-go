e36801669442923f72551d9174e0ea819d6f65e5
lixiaohui <lixiaohui0812@gmail.com>
2020-04-13 11:49:00 +0800

    [xlog] update xlog

--- xlog/log.go
+++ xlog/log.go
@@ -5,6 +5,7 @@ import (
 	"io"
 )
 
+// Logger is abstraction of log behavior.
 type Logger interface {
 	Print(args ...interface{})
 	Printf(format string, args ...interface{})
@@ -22,8 +23,19 @@ func (w *writerLogger) Printf(format string, args ...interface{}) {
 	_, _ = w.w.Write([]byte(fmt.Sprintf(format, args...)))
 }
 
+// Writer return a Logger which wrap an io.Writer.
 func Writer(w io.Writer) Logger {
 	return &writerLogger{w: w}
+}
+
+// MultiWriter return a Logger which write data to multiply io.Writer.
+func MultiWriter(ws ...io.Writer) Logger {
+	if len(ws) == 0 {
+		return Nop()
+	} else if len(ws) == 1 {
+		return Writer(ws[0])
+	}
+	return &writerLogger{w: io.MultiWriter(ws...)}
 }
 
 type nopLogger struct{}
@@ -31,4 +43,5 @@ type nopLogger struct{}
 func (nopLogger) Print(args ...interface{})                 {}
 func (nopLogger) Printf(format string, args ...interface{}) {}
 
+// Nop return a Logger which do nothing.
 func Nop() Logger { return nopLogger{} }
