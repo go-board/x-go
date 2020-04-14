8d395104e3b7b776587b7a7d1e2ff11ca4af3e5d
lixiaohui <lixiaohui0812@gmail.com>
2020-04-14 12:05:53 +0800

    [xcontainer] add delay_queue

---
+++ xcontainer/delay_queue/delay_queue.go
@@ -0,0 +1,77 @@
+package delay_queue
+
+import (
+	"time"
+
+	"github.com/go-board/x-go/types"
+	"github.com/go-board/x-go/xcontainer/priority_queue"
+)
+
+type Task struct {
+	data interface{}
+	at   time.Time
+}
+
+func (t Task) Compare(o types.Comparable) types.Ordering {
+	oo := o.(*Task)
+	if t.at.Before(oo.at) {
+		return types.OrderingLess
+	}
+	if t.at.Equal(oo.at) {
+		return types.OrderingEqual
+	}
+	return types.OrderingGreater
+}
+
+func NewTask(v interface{}, at time.Time) *Task {
+	return &Task{data: v, at: at}
+}
+
+func NewDelayTask(v interface{}, d time.Duration) *Task {
+	return NewTask(v, time.Now().Add(d))
+}
+
+type DelayQueue struct {
+	q      *priority_queue.PriorityQueue
+	notify chan struct{}
+}
+
+func New() *DelayQueue {
+	return &DelayQueue{
+		q:      priority_queue.NewComparablePriorityQueue(false),
+		notify: make(chan struct{}, 1),
+	}
+}
+
+// Push new data into delay queue.
+func (q *DelayQueue) Push(task *Task) {
+	q.q.Push(task)
+}
+
+// Pop try to pop nearest expired data.
+func (q *DelayQueue) Pop() (interface{}, bool) {
+	task := q.q.Pop()
+	if task == nil {
+		return nil, false
+	}
+	t := task.(*Task)
+	if !t.at.After(time.Now()) {
+		return t.data, true
+	}
+	q.q.Push(task)
+	return nil, false
+}
+
+// BlockPop must get a data otherwise wait for data ready.
+func (q *DelayQueue) BlockPop() interface{} {
+	for {
+		// todo: optimization CPU usage
+		//   1. try pop, if got, return, else go to step 2
+		//   2. get task awake time, and block on it, and listen on notify channel for pushing new data
+		//   3. if timeout or receive notification, go to step 1
+		v, ok := q.Pop()
+		if ok {
+			return v
+		}
+	}
+}
