c215e7a32ec29a15336103ee079b85e8f649485e
lixiaohui <lixiaohui0812@gmail.com>
2020-06-03 09:52:39 +0800

    [xstrings] add MutableString

---
+++ xstrings/mutable_string.go
@@ -0,0 +1,140 @@
+package xstrings
+
+import (
+	"io"
+)
+
+// MutableString is the mutable version of const string
+type MutableString struct {
+	vec []uint8
+}
+
+// FromString create MutableString from const string
+func FromString(s string) *MutableString {
+	vec := make([]byte, len(s), len(s))
+	copy(vec, s)
+	return &MutableString{vec: vec}
+}
+
+// WithCapacity create MutableString with given capacity.
+func WithCapacity(cap int) *MutableString {
+	return &MutableString{vec: make([]uint8, 0, cap)}
+}
+
+// ToString convert MutableString to const string
+func (s *MutableString) ToString() string { return string(s.vec) }
+
+// AppendString append string at tail of MutableString
+func (s *MutableString) AppendString(str string) { s.vec = append(s.vec, str...) }
+
+// AppendByte append a byte at tail of MutableString
+func (s *MutableString) AppendByte(b uint8) { s.vec = append(s.vec, b) }
+
+// AppendBytes append bytes at tail of MutableString
+func (s *MutableString) AppendBytes(bytes []uint8) {
+	s.vec = append(s.vec, bytes...)
+}
+
+func (s *MutableString) AsBytes() []uint8 {
+	b := make([]uint8, 0, len(s.vec))
+	copy(b, s.vec)
+	return b
+}
+
+func (s *MutableString) Insert(idx int, b uint8) {
+	if idx < 0 || idx > len(s.vec) {
+		panic("")
+	}
+	if cap(s.vec) > len(s.vec) {
+		s.vec = s.vec[:len(s.vec)+1]
+		copy(s.vec[idx+1:], s.vec[idx:])
+		s.vec[idx] = b
+		return
+	}
+	newVec := make([]uint8, len(s.vec)+1)
+	copy(newVec, s.vec[:idx])
+	newVec[idx] = b
+	copy(newVec[idx+1:], s.vec[idx:])
+	s.vec = newVec
+}
+
+func (s *MutableString) InsertString(idx int, str string) {
+	if idx < 0 || idx > len(s.vec) {
+		panic("")
+	}
+	if cap(s.vec) >= len(s.vec)+len(str) {
+		s.vec = s.vec[:len(s.vec)+len(str)]
+		copy(s.vec[idx+len(str):], s.vec[idx:])
+		copy(s.vec[idx:], str)
+		return
+	}
+	newVec := make([]uint8, len(s.vec)+len(str))
+	copy(newVec, s.vec[:idx])
+	copy(newVec[idx:], str)
+	copy(newVec[idx+len(str):], s.vec[idx:])
+	s.vec = newVec
+}
+
+func (s *MutableString) Remove(idx int) {
+	if idx < 0 || idx >= len(s.vec) {
+		panic("err: index ouf of range")
+	}
+	copy(s.vec[idx:], s.vec[idx+1:])
+	s.vec = s.vec[:len(s.vec)-1]
+}
+
+func (s *MutableString) RemoveRange(start, length int) {
+	if start < 0 || length <= 0 || start > len(s.vec) || start+length > len(s.vec) {
+		panic("err: index ouf of range")
+	}
+	copy(s.vec[start:], s.vec[start+length:])
+	s.vec = s.vec[:len(s.vec)-length]
+}
+
+func (s *MutableString) Replace(idx int, b uint8) {
+	if idx < 0 || idx > len(s.vec) {
+		panic("err: index ouf of range")
+	}
+	s.vec[idx] = b
+}
+
+func (s *MutableString) ReplaceRange(idx int, bytes []uint8) {
+	if idx < 0 || idx > len(s.vec) || idx+len(bytes) > len(s.vec) {
+		panic("err: index ouf of range")
+	}
+	copy(s.vec[idx:], bytes)
+}
+
+func (s *MutableString) Length() int { return len(s.vec) }
+
+func (s *MutableString) Capacity() int { return cap(s.vec) }
+
+func (s *MutableString) First() (uint8, bool) {
+	if len(s.vec) > 0 {
+		return s.vec[0], true
+	}
+	return 0, false
+}
+
+func (s *MutableString) Last() (uint8, bool) {
+	if len(s.vec) > 0 {
+		return s.vec[len(s.vec)-1], true
+	}
+	return 0, false
+}
+
+func (s *MutableString) Read(p []byte) error {
+	if len(p) > len(s.vec) {
+		copy(p, s.vec)
+		s.vec = s.vec[:0]
+		return io.ErrUnexpectedEOF
+	}
+	copy(p, s.vec)
+	s.vec = s.vec[len(p):]
+	return nil
+}
+
+func (s *MutableString) Write(p []byte) (n int, err error) {
+	s.AppendBytes(p)
+	return len(p), nil
+}
---
+++ xstrings/mutable_string_test.go
@@ -0,0 +1,59 @@
+package xstrings
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMutableStringToString(t *testing.T) {
+	s := FromString("Hello,world")
+	require.Equal(t, "Hello,world", s.ToString())
+}
+
+func TestMutableStringInsert(t *testing.T) {
+	t.Run("Insert", func(t *testing.T) {
+		s := FromString("Hello,world")
+		s.Insert(0, '!')
+		require.Equal(t, "!Hello,world", s.ToString())
+	})
+	t.Run("InsertString", func(t *testing.T) {
+		s := FromString("Hello,")
+		s.InsertString(s.Len(), "world")
+		require.Equal(t, "Hello,world", s.ToString())
+	})
+}
+
+func TestMutableStringRemove(t *testing.T) {
+	t.Run("Remove", func(t *testing.T) {
+		s := FromString("Hello,world")
+		s.Remove(0)
+		require.Equal(t, "ello,world", s.ToString())
+		s.Remove(3)
+		require.Equal(t, "ell,world", s.ToString())
+	})
+	t.Run("RemoveRange", func(t *testing.T) {
+		s := FromString("Hello,world")
+		s.RemoveRange(0, 2)
+		require.Equal(t, "llo,world", s.ToString())
+		s.RemoveRange(3, 3)
+		require.Equal(t, "llorld", s.ToString())
+	})
+}
+
+func TestMutableStringReplace(t *testing.T) {
+	t.Run("Replace", func(t *testing.T) {
+		s := FromString("Hello,world")
+		s.Replace(0, 'A')
+		require.Equal(t, "Aello,world", s.ToString())
+		s.Replace(2, 'b')
+		require.Equal(t, "Aeblo,world", s.ToString())
+	})
+	t.Run("ReplaceRange", func(t *testing.T) {
+		s := FromString("Hello,world")
+		s.ReplaceRange(0, []byte{'W', 'o', 'r', 'l', 'd'})
+		require.Equal(t, "World,world", s.ToString())
+		s.ReplaceRange(6, []byte{'h', 'e', 'l', 'l', 'o'})
+		require.Equal(t, "World,hello", s.ToString())
+	})
+}
